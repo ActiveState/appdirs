fa336159412d3b47d12691c824331104a6863262
Sridhar Ratnakumar <sridhar.ratna@gmail.com>
2010-08-31 17:17:43 -0700

    Add a setup.py script, so others can 'pip install' this project

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from setuptools import setup, find_packages
+
+
+
+_top_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(_top_dir, "lib"))
+try:
+    import appdirs
+finally:
+    del sys.path[0]
+README = open(os.path.join(_top_dir, 'README.md')).read()
+
+setup(name='appdirs',
+    version=appdirs.__version__,
+    description='A small Python module for determining appropriate platform-specific dirs, e.g. a "user data dir".',
+    long_description=README,
+    classifiers=[c.strip() for c in """
+        Development Status :: 4 - Beta
+        Intended Audience :: Developers
+        License :: OSI Approved :: MIT License
+        Operating System :: OS Independent
+        Programming Language :: Python :: 2
+        Programming Language :: Python :: 2.4
+        Programming Language :: Python :: 2.5
+        Programming Language :: Python :: 2.6
+        Programming Language :: Python :: 2.7
+        Programming Language :: Python :: 3
+        Topic :: Software Development :: Libraries :: Python Modules
+        """.split('\n') if c.strip()],
+    author='Trent Mick',
+    author_email='trentm@gmail.com',
+    maintainer='Trent Mick',
+    maintainer_email='trentm@gmail.com',
+    url='http://github.com/ActiveState/appdirs',
+    license='MIT',
+    py_modules=["appdirs"],
+    package_dir={"": "lib"},
+    include_package_data=True,
+    zip_safe=False,
+)
