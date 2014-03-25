f6450f3bda3e42cd4c55bd026824c59764ed7d75
Benjamin Drung <benjamin.drung@profitbricks.com>
2014-03-25 12:03:15 +0100

    Compare results with basestring on Python 2 (Fixes #27).

--- test/test_api.py
+++ test/test_api.py
@@ -1,5 +1,11 @@
 import unittest
+import sys
 import appdirs
+
+if sys.version_info[0] < 3:
+    STRING_TYPE = basestring
+else:
+    STRING_TYPE = str
 
 class Test_AppDir(unittest.TestCase):
     def test_metadata(self):
@@ -8,20 +14,20 @@ class Test_AppDir(unittest.TestCase):
 
     def test_helpers(self):
         self.assertIsInstance(
-            appdirs.user_data_dir('MyApp', 'MyCompany'), str)
+            appdirs.user_data_dir('MyApp', 'MyCompany'), STRING_TYPE)
         self.assertIsInstance(
-            appdirs.site_data_dir('MyApp', 'MyCompany'), str)
+            appdirs.site_data_dir('MyApp', 'MyCompany'), STRING_TYPE)
         self.assertIsInstance(
-            appdirs.user_cache_dir('MyApp', 'MyCompany'), str)
+            appdirs.user_cache_dir('MyApp', 'MyCompany'), STRING_TYPE)
         self.assertIsInstance(
-            appdirs.user_log_dir('MyApp', 'MyCompany'), str)
+            appdirs.user_log_dir('MyApp', 'MyCompany'), STRING_TYPE)
 
     def test_dirs(self):
         dirs = appdirs.AppDirs('MyApp', 'MyCompany', version='1.0')
-        self.assertIsInstance(dirs.user_data_dir, str)
-        self.assertIsInstance(dirs.site_data_dir, str)
-        self.assertIsInstance(dirs.user_cache_dir, str)
-        self.assertIsInstance(dirs.user_log_dir, str)
+        self.assertIsInstance(dirs.user_data_dir, STRING_TYPE)
+        self.assertIsInstance(dirs.site_data_dir, STRING_TYPE)
+        self.assertIsInstance(dirs.user_cache_dir, STRING_TYPE)
+        self.assertIsInstance(dirs.user_log_dir, STRING_TYPE)
 
 if __name__=="__main__":
     unittest.main()
