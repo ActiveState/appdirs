79eeb2df9e37532eb6b55b893e6f4fb26946036e
KOLANICH <kolan_n@mail.ru>
2019-11-14 10:51:33 +0300

    Moved the metadata into `setup.cfg`. Added pyproject.toml to allow building with `build`.

--- setup.py
+++ setup.py
@@ -7,52 +7,19 @@ except ImportError:
     from distutils.core import setup
 import ast
 
-
+current_dir = os.path.dirname(__file__)
 def read(fname):
-    inf = open(os.path.join(os.path.dirname(__file__), fname))
+    inf = open(os.path.join(current_dir, fname), "rt", encoding="utf-8")
     out = "\n" + inf.read().replace("\r\n", "\n")
     inf.close()
     return out
 
+if __name__ == "__main__":
+    # Do not import `appdirs` yet, lest we import some random version on sys.path.
+    for line in read("appdirs.py").splitlines():
+        if line.startswith("__version__"):
+            version = ast.literal_eval(line.split("=", 1)[1].strip())
+            break
 
-# Do not import `appdirs` yet, lest we import some random version on sys.path.
-for line in read("appdirs.py").splitlines():
-    if line.startswith("__version__"):
-        version = ast.literal_eval(line.split("=", 1)[1].strip())
-        break
 
-
-setup(
-    name='appdirs',
-    version=version,
-    description='A small Python module for determining appropriate ' + \
-        'platform-specific dirs, e.g. a "user data dir".',
-    long_description=read('README.rst') + '\n' + read('CHANGES.rst'),
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    keywords='application directory log cache user',
-    author='Trent Mick',
-    author_email='trentm@gmail.com',
-    maintainer='Jeff Rouse',
-    maintainer_email='jr@its.to',
-    url='https://github.com/ActiveState/appdirs',
-    license='MIT',
-    py_modules=["appdirs"],
-)
+    setup(version=version)
